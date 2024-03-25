88797e89ab5a5748d660deeb1bb4fede37d3f435
Muchammad Ramadhan <muchammadramadhan12@gmail.com>
2024-03-25 11:49:27 +0700

    first commit

--- app/Component/BoxAbout.js
+++ app/Component/BoxAbout.js
@@ -21,7 +21,6 @@ export default function BoxAbout({ title, paragraph }) {
   const { CreateProfile } = useAuth();
   const router = useRouter();
 
-
   useEffect(() => {
     const storedUsername = localStorage.getItem("username");
     if (storedUsername) {
@@ -97,29 +96,33 @@ export default function BoxAbout({ title, paragraph }) {
         {editMode ? (
           <div className={`Box-biodata-edit relative`}>
             <div className="content text-black absolute">
-              {bio? ( <div className="tag-about flex justify-between items-center text-white">
-                <h2>{title}</h2>
-                <div className="cursor-pointer icon-edit" onClick={handleEditBio}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 00 .196l6.813-6.814z" />
-                    <path d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                  </svg>
+              {bio ? (
+                <div className="tag-about flex justify-between items-center text-white">
+                  <h2>{title}</h2>
+                  <div className="cursor-pointer icon-edit" onClick={handleEditBio}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 00 .196l6.813-6.814z" />
+                      <path d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                    </svg>
+                  </div>
                 </div>
-              </div>):( <div className="tag-about flex justify-between items-center text-white">
-                <h2>{title}</h2>
-                <div className="cursor-pointer" onClick={handleSave}>
-                  <h2 className="text-orange-300">Save & Update</h2>
+              ) : (
+                <div className="tag-about flex justify-between items-center text-white">
+                  <h2>{title}</h2>
+                  <div className="cursor-pointer" onClick={handleSave}>
+                    <h2 className="text-orange-300">Save & Update</h2>
+                  </div>
                 </div>
-              </div>)}
-             
+              )}
+
               <div className="image-side mt-7 flex items-center">
                 <label htmlFor="uploadImage" className="image-profile bg-gray-500 mr-5 flex justify-center items-center rounded-lg">
                   {imageSrc ? (
                     <img src={imageSrc} alt="Uploaded" className="w-full h-full object-cover rounded-lg" />
                   ) : (
                     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="white" class="bi bi-plus-lg" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
-</svg>
+                      <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+                    </svg>
                   )}
                   <input type="file" id="uploadImage" className="hidden" onChange={handleImageChange} />
                 </label>
