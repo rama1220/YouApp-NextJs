79bd19a86ba74bb1970061e23bbf3109398fcee6
Muchammad Ramadhan <muchammadramadhan12@gmail.com>
2024-03-27 16:39:00 +0700

    fisrt commit

--- app/Component/BoxAbout.js
+++ app/Component/BoxAbout.js
@@ -19,11 +19,12 @@ export default function BoxAbout({ title, paragraph }) {
   const [imageSrc, setImageSrc] = useState(null);
   const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
   const { CreateProfile, Born, Age } = useAuth();
-  const [classBio, setClass] = useState("");
   const router = useRouter();
 
   const yourBirthday = Born(formData.birthday);
   const yourAge = Age(formData.birthday);
+
+
 
   useEffect(() => {
     const storedUsername = localStorage.getItem("username");
@@ -55,7 +56,6 @@ export default function BoxAbout({ title, paragraph }) {
 
   const handleEditBio = () => {
     setBio(false);
-    setClass("Box-biodata-edit");
   };
   useEffect(() => {
     const imageProfile = localStorage.getItem("image");
@@ -80,7 +80,6 @@ export default function BoxAbout({ title, paragraph }) {
       });
       setEditMode(true);
       setBio(true);
-      setClass("bioBox");
     }
   }, []);
 
@@ -99,7 +98,6 @@ export default function BoxAbout({ title, paragraph }) {
         localStorage.setItem("weight", formData.weight);
       }
       setBio(true);
-      setClass("bioBox");
       const height = parseFloat(formData.height);
       const weight = parseFloat(formData.weight);
 
@@ -142,14 +140,14 @@ export default function BoxAbout({ title, paragraph }) {
       </div>
       <div className="content-container">
         {editMode ? (
-          <div className={`${classBio} relative`}>
+          <div className={`Box-biodata-edit relative`}>
             <div className="content text-black absolute">
               {bio ? (
                 <div className="tag-about flex justify-between items-center text-white">
                   <h2>{title}</h2>
                   <div className="cursor-pointer icon-edit" onClick={handleEditBio}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.80 5a.5.5 0 00 .196l6.813-6.814z" />
                       <path d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
                     </svg>
                   </div>
